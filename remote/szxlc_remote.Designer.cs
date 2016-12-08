d9be99d8acb6fc77c86026631c71bc74daf04128
广渝 陈 <chen_zhizhon@163.com>
2016-12-08 23:24:12 +0800

    添加项目文件。

---
+++ remote/szxlc_remote.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace szxlc.remote
+{
+    partial class FrmRemote
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRemote));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLine1 = new System.Windows.Forms.Button();
+            this.btnLine2 = new System.Windows.Forms.Button();
+            this.btnLine3 = new System.Windows.Forms.Button();
+            this.btnLine4 = new System.Windows.Forms.Button();
+            this.btnLine5 = new System.Windows.Forms.Button();
+            this.btnLine6 = new System.Windows.Forms.Button();
+            this.btnLine7 = new System.Windows.Forms.Button();
+            this.btnLine8 = new System.Windows.Forms.Button();
+            this.btnLine9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(437, 95);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLine1
+            // 
+            this.btnLine1.Enabled = false;
+            this.btnLine1.Location = new System.Drawing.Point(12, 101);
+            this.btnLine1.Name = "btnLine1";
+            this.btnLine1.Size = new System.Drawing.Size(75, 23);
+            this.btnLine1.TabIndex = 1;
+            this.btnLine1.Text = "一号线";
+            this.btnLine1.UseVisualStyleBackColor = true;
+            // 
+            // btnLine2
+            // 
+            this.btnLine2.Enabled = false;
+            this.btnLine2.Location = new System.Drawing.Point(169, 101);
+            this.btnLine2.Name = "btnLine2";
+            this.btnLine2.Size = new System.Drawing.Size(75, 23);
+            this.btnLine2.TabIndex = 2;
+            this.btnLine2.Text = "二号线";
+            this.btnLine2.UseVisualStyleBackColor = true;
+            // 
+            // btnLine3
+            // 
+            this.btnLine3.Enabled = false;
+            this.btnLine3.Location = new System.Drawing.Point(324, 101);
+            this.btnLine3.Name = "btnLine3";
+            this.btnLine3.Size = new System.Drawing.Size(75, 23);
+            this.btnLine3.TabIndex = 3;
+            this.btnLine3.Text = "三号线";
+            this.btnLine3.UseVisualStyleBackColor = true;
+            this.btnLine3.Click += new System.EventHandler(this.btnLine3_Click);
+            // 
+            // btnLine4
+            // 
+            this.btnLine4.Enabled = false;
+            this.btnLine4.Location = new System.Drawing.Point(12, 130);
+            this.btnLine4.Name = "btnLine4";
+            this.btnLine4.Size = new System.Drawing.Size(75, 23);
+            this.btnLine4.TabIndex = 4;
+            this.btnLine4.Text = "四号线";
+            this.btnLine4.UseVisualStyleBackColor = true;
+            // 
+            // btnLine5
+            // 
+            this.btnLine5.Enabled = false;
+            this.btnLine5.Location = new System.Drawing.Point(169, 130);
+            this.btnLine5.Name = "btnLine5";
+            this.btnLine5.Size = new System.Drawing.Size(75, 23);
+            this.btnLine5.TabIndex = 5;
+            this.btnLine5.Text = "五号线";
+            this.btnLine5.UseVisualStyleBackColor = true;
+            // 
+            // btnLine6
+            // 
+            this.btnLine6.Enabled = false;
+            this.btnLine6.Location = new System.Drawing.Point(324, 130);
+            this.btnLine6.Name = "btnLine6";
+            this.btnLine6.Size = new System.Drawing.Size(75, 23);
+            this.btnLine6.TabIndex = 6;
+            this.btnLine6.Text = "六号线";
+            this.btnLine6.UseVisualStyleBackColor = true;
+            // 
+            // btnLine7
+            // 
+            this.btnLine7.Enabled = false;
+            this.btnLine7.Location = new System.Drawing.Point(12, 160);
+            this.btnLine7.Name = "btnLine7";
+            this.btnLine7.Size = new System.Drawing.Size(75, 23);
+            this.btnLine7.TabIndex = 7;
+            this.btnLine7.Text = "七号线";
+            this.btnLine7.UseVisualStyleBackColor = true;
+            // 
+            // btnLine8
+            // 
+            this.btnLine8.Enabled = false;
+            this.btnLine8.Location = new System.Drawing.Point(169, 160);
+            this.btnLine8.Name = "btnLine8";
+            this.btnLine8.Size = new System.Drawing.Size(75, 23);
+            this.btnLine8.TabIndex = 8;
+            this.btnLine8.Text = "八号线";
+            this.btnLine8.UseVisualStyleBackColor = true;
+            // 
+            // btnLine9
+            // 
+            this.btnLine9.Enabled = false;
+            this.btnLine9.Location = new System.Drawing.Point(324, 160);
+            this.btnLine9.Name = "btnLine9";
+            this.btnLine9.Size = new System.Drawing.Size(75, 23);
+            this.btnLine9.TabIndex = 9;
+            this.btnLine9.Text = "九号线";
+            this.btnLine9.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1、线路中灰色表示离线!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(407, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "2、请打开一个记事本把问题描述在记事本上再发远程。最好加上联系电话！";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "3、当外网不通，发不了远程时，请电：4000110075";
+            // 
+            // FrmRemote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLine9);
+            this.Controls.Add(this.btnLine8);
+            this.Controls.Add(this.btnLine7);
+            this.Controls.Add(this.btnLine6);
+            this.Controls.Add(this.btnLine5);
+            this.Controls.Add(this.btnLine4);
+            this.Controls.Add(this.btnLine3);
+            this.Controls.Add(this.btnLine2);
+            this.Controls.Add(this.btnLine1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FrmRemote";
+            this.Text = "深圳鑫联创远程维护工具";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmRemote_FormClosing);
+            this.Load += new System.EventHandler(this.remote_Load);
+            this.SizeChanged += new System.EventHandler(this.FrmRemote_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLine1;
+        private System.Windows.Forms.Button btnLine2;
+        private System.Windows.Forms.Button btnLine3;
+        private System.Windows.Forms.Button btnLine4;
+        private System.Windows.Forms.Button btnLine5;
+        private System.Windows.Forms.Button btnLine6;
+        private System.Windows.Forms.Button btnLine7;
+        private System.Windows.Forms.Button btnLine8;
+        private System.Windows.Forms.Button btnLine9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ remote/szxlc_remote.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szxlc.remote
+{
+    public partial class FrmRemote : Form
+    {
+        BackgroundWorker BW;
+        static string companyURL = Properties.Resources.companyURL;
+        IPAddress ipaddr = (IPAddress)Dns.GetHostEntry(companyURL).AddressList.GetValue(0);
+        int startPort = int.Parse(Properties.Resources.remoteStartPort);
+        int endPort = int.Parse(Properties.Resources.remoteEndPort);
+        public FrmRemote()
+        {
+            InitializeComponent();
+            InitializeBackgroudWorker();
+
+        }
+
+        private void InitializeBackgroudWorker()
+        {
+
+            BW = new BackgroundWorker();
+            BW.WorkerSupportsCancellation = true;
+            BW.WorkerReportsProgress = true;
+            BW.DoWork += new DoWorkEventHandler(BW_DoWork);
+            BW.ProgressChanged += new ProgressChangedEventHandler(BW_ProgressChanged);
+        }
+
+        private void BW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //BackgroundWorker bwtemp = sender as BackgroundWorker;
+            //portstatus p = (portstatus)e.Argument;
+            for (int i = startPort; i <=endPort; i++)
+            {
+                if (BW.CancellationPending)
+                {
+                    e.Cancel = true;
+                }
+               bool  status =IsPortOpen(ipaddr, i);
+                BW.ReportProgress(i, status);
+            }
+
+            
+           
+        }
+
+        private void BW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            int p = e.ProgressPercentage;
+            bool status = bool.Parse(e.UserState.ToString());
+            int line = p % startPort + 1;
+            string btnName = "btnLine" + line;
+            if(FindControl(Controls.Owner,btnName)!=null)
+            {
+                Button btn = (Button)this.FindControl(Controls.Owner, btnName);
+                btn.Enabled = bool.Parse(e.UserState.ToString());
+                btn.Update();
+            }
+           
+           
+        }
+        private Control FindControl(Control control,string controlName)
+        {
+            Control c1;
+            foreach (Control c in control.Controls)
+            {
+                if (c.Name == controlName)
+                {
+                    return c;
+                }
+                else if (c.Controls.Count > 0)
+                {
+                    c1 = FindControl(c, controlName);
+                    if (c1 != null)
+                    {
+                        return c1;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void remote_Load(object sender, EventArgs e)
+        {
+            PortScan();
+
+        }
+
+        private void PortScan()
+        {
+
+            //portstatus ps = new portstatus();
+            //ps.port = startPort;
+
+            //while (ps.port <= endPort)
+            //{
+                BW.RunWorkerAsync();
+            //    ps.port++;
+            //}
+        }
+        private   bool IsPortOpen(IPAddress ip, int port)       //判断某一计算机端口是否处于打开状态
+        {
+            try
+            {
+                TcpClient client = new TcpClient();       //创建一个TcpClient实例
+                client.ReceiveTimeout = client.SendTimeout = 100;
+                //IPAddress address = IPAddress.Parse(ip);  //转化string类型的ip地址到IpAddress
+                client.Connect(ip, port);            //连接服务器address的port端口
+                client.Close();                           //连接成功立即断开
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private void FrmRemote_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(BW.IsBusy)
+            { BW.CancelAsync(); }
+            
+        }
+
+        private void FrmRemote_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                if (BW.IsBusy)
+                { BW.CancelAsync(); }
+            }
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                if (!BW.IsBusy)
+                { 
+                PortScan();
+            }
+            }
+            
+        }
+
+        private void btnLine3_Click(object sender, EventArgs e)
+        {
+            //三号线被点击
+            //关闭端口扫描;
+            //最小化主窗口；
+            //
+        }
+    }
+    //public class portstatus
+    //{
+    //    public int port
+    //    { get; set; }
+    //    public bool status
+    //    { get; set; }
+    //}
+}
